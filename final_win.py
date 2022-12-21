460662c6bcc2e511c9a8ec799ad788fee9301bd1
stogegor <118925956+stogegor@users.noreply.github.com>
2022-12-21 20:28:14 +0300

    Add files via upload

--- final_win.py
+++ final_win.py
@@ -2,9 +2,11 @@
 from instr import *
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout
-class Mainwin(QWidget):
-    def __init__(self):
+class Thirdwin(QWidget):
+    def __init__(self, exp):
         super().__init__()
+        self.exp = exp
+        self.index 
         self.set_appear()
         self.initUI()
         self.result()
@@ -17,4 +19,62 @@ class Mainwin(QWidget):
         self.work_text = QLabel(txt_workheart)
         self.index_text = QLabel(txt_index)
     def result(self):
-        index = (4*(int(self.exp.t1)+int(self.exp.t2)+int(self.exp.t3))-200)/10
+        self.index = (4*(int(self.exp.t1)+int(self.exp.t2)+int(self.exp.t3))-200)/10
+        if self.exp.age >= 15:
+            if self.index >= 15:
+                return txt_res1
+            elif self.index >= 11 and self.index < 15:
+                return txt_res2
+            elif self.index >=6 and self.index < 11:
+                return txt_res3
+            elif self.index >=0.5 and self.index < 6:
+                return txt_res4
+            elif self.index < 0.5
+                return txt_res5
+        elif self.exp.age < 15 and self.exp.age >= 13:
+            if self.index >= 16.5:
+                return txt_res1
+            elif self.index >=12.5 and self.index < 16.5:
+                return txt_res2
+            elif self.index >=7.5 and self.index < 12.5:
+                return txt_res3
+            elif self.index >=2 and self.index < 7.5:
+                return txt_res4
+            elif self.index < 2:
+                return txt_res5
+        elif self.exp.age < 13 and self.exp.age >= 11:
+            if self.index >= 18:
+                return txt_res1
+            elif self.index >=14 and self.index < 18:
+                return txt_res2
+            elif self.index >=9 and self.index < 14:
+                return txt_res3
+            elif self.index >=3.5 and self.index < 9:
+                return txt_res4
+            elif self.index < 3.5:
+                return txt_res5
+        elif self.exp.age < 11 and self.exp.age >= 9:
+            if self.index >= 19.5:
+                return txt_res1
+            elif self.index >=15.5 and self.index < 19.5:
+                return txt_res2
+            elif self.index >= 10.5 and self.index < 15.5:
+                return txt_res3
+            elif self.index >=5 and self.index < 10.5:
+                return txt_res4
+            elif self.index < 5:
+                return txt_res5
+        elif self.exp.age < 9 and self.exp.age >= 7:
+            if self.index >= 21:
+                return txt_res1
+            elif self.index >=17 and self.index < 21:
+                return txt_res2
+            elif self.index >=12 and self.index < 17:
+                return txt_res3
+            elif self.index >=6.5 and self.index < 12:
+                return txt_res4
+            elif self.index < 6.5:
+                return txt_res5
+            
+        
+        
